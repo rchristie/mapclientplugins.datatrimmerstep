949718852e6bd8124c0f318fb940867811807943
mahyar-osn <m.osanlouy@auckland.ac.nz>
2020-05-02 12:51:38 +1200

    methods to get group fields and destroy them + small changes

--- mapclientplugins/datatrimmerstep/model/datamodel.py
+++ mapclientplugins/datatrimmerstep/model/datamodel.py
@@ -1,9 +1,9 @@
+import os
+
 from opencmiss.zinc.context import Context
 from opencmiss.zinc.field import Field
 from opencmiss.zinc.glyph import Glyph
 from opencmiss.zinc.status import OK as ZINC_OK
-from opencmiss.zinc.material import Material
-from opencmiss.utils.zinc.field import getGroupList
 from opencmiss.utils.zinc.general import ChangeManager
 
 from math import sqrt
@@ -24,19 +24,37 @@ class DataModel(object):
         self._region = self._context.createRegion()
         self._region.setName('TrimRegion')
         self._field_module = self._region.getFieldmodule()
+        self._ex_filename = ex_data_file
+        self._output_filename = None
         #  read the EX Zinc data file
         self._coordinate_field = None
         self._groups = None
+        self._group_fields = None
+        self._group_dct = None
         self._initialise_ex_data(ex_data_file)
         self._material_module = self._context.getMaterialmodule()
         self._initialise_glyph_material()
         self._initialise_tessellation(12)
+        self._scene_change_callback = None
 
         self._initialise_scene()
 
     def _discover_groups(self):
         with ChangeManager(self._field_module):
-            self._groups = [group.getName() for group in getGroupList(self._field_module)]
+            self._group_fields, self._groups = self._get_group_fields()
+
+    def _get_group_fields(self):
+        group_fields = []
+        groups = []
+        field_iter = self._field_module.createFielditerator()
+        field = field_iter.next()
+        while field.isValid():
+            group = field.castGroup()
+            if group.isValid():
+                group_fields.append(field)
+                groups.append(group)
+            field = field_iter.next()
+        return group_fields, groups
 
     def _get_data_coordinate_field(self):
         node_point_set = self._field_module.findNodesetByFieldDomainType(Field.DOMAIN_TYPE_NODES)
@@ -101,13 +119,18 @@ class DataModel(object):
         self._coordinate_field = self._get_data_coordinate_field()
         self._discover_groups()
 
-    def _create_rgb_spectrum(self):
-        field = self._field_module.findFieldByName('rgb')
-        spectrummodule = self._scene.getSpectrummodule()
-        spectrum = None
-        # dataProjections.setSpectrum(spectrum)
-        # dataProjections.setName("displayDataProjections")
-        # dataProjections.setVisibilityFlag(self.isDisplayDataProjections())
+    # def _create_rgb_spectrum(self):
+    #     field = self._field_module.findFieldByName('rgb')
+    #     spectrummodule = self._scene.getSpectrummodule()
+    #     spectrum = None
+    #     dataProjections.setSpectrum(spectrum)
+    #     dataProjections.setName("displayDataProjections")
+    #     dataProjections.setVisibilityFlag(self.isDisplayDataProjections())
+
+    def generate(self):
+        self.create_graphics()
+        if self._scene_change_callback:
+            self._scene_change_callback()
 
     def create_graphics(self):
         # self._create_rgb_spectrum()
@@ -121,14 +144,41 @@ class DataModel(object):
         points.setMaterial(self._material_module.findMaterialByName('silver'))
         points.setName('display_points')
 
+    def destroy_groups(self, groups: []):
+        if groups is None:
+            return
+        mesh1d = self._field_module.findMeshByDimension(1)
+        nodeset = self._field_module.findNodesetByFieldDomainType(Field.DOMAIN_TYPE_NODES)
+        with ChangeManager(self._field_module):
+            for group in groups:
+                if group in self._group_dct.keys():
+                    mesh1d.destroyElementsConditional(self._group_dct[group])
+                    nodeset.destroyNodesConditional(self._group_dct[group])
+                    self._group_dct[group].setManaged(False)
+                    del self._group_dct[group]
+
     def get_context(self):
         return self._context
 
-    def get_groups(self):
-        return self._groups
+    def get_group_names(self):
+        group_names = [group.getName() for group in self._groups]
+        self._group_dct = dict(zip(group_names, self._groups))
+        return group_names
+
+    def get_output_filename(self):
+        return self._output_filename
 
     def get_region(self):
         return self._region
 
     def get_scene(self):
         return self._region.getScene()
+
+    def register_scene_change_callback(self, scene_change_callback):
+        self._scene_change_callback = scene_change_callback
+
+    def write_model(self):
+        filename = os.path.basename(self._ex_filename).split('.')[0] + '_Trimmed.ex'
+        path = os.path.dirname(self._ex_filename)
+        self._output_filename = os.path.join(path, filename)
+        self._region.writeFile(self._output_filename)
